4d7ade411c9551706e77a6bc75af9d05aac8cb19
Chris <cjr1977@hotmail.co.uk>
2021-12-05 11:31:15 +0000

    Update table, gitignore and scrape.py

---
+++ SugarWOD/sugarwod_scrape.py
@@ -0,0 +1,8 @@
+# ====== LOGIN_TO_SUGARWOD_&_SAVE_PAGES ====== #
+# The following code logs into SugarWOD, loads and saves calendar webpages as
+# html files so that they can later be opened and parsed. The script creates a 
+# list of all weeks which have not already been saved to the HTML_files folder,
+# then sequential loads and saves these pages.
+
+# Scrape dynamic webpage without saving html files....
+# IN PROGRESS
